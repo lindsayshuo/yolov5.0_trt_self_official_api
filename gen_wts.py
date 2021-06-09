afa7e7f7c5909e0d79358af69b48af8ce3e3f67e
lindsayshuo <45239466+lindsayshuo@users.noreply.github.com>
2021-06-09 15:40:19 +0800

    Update gen_wts.py

--- gen_wts.py
+++ gen_wts.py
@@ -1,30 +1,68 @@
 import torch
 import struct
 import sys
-from utils.torch_utils import select_device
+import platform
+import subprocess
+import datetime
+import os
+from pathlib import Path
+import logging
 
-# Initialize
-device = select_device('cpu')
-pt_file = "yolov5s.pt" # sys.argv[1]
-# Load model
-model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
-model.to(device).eval()
 
-# print(model.state_dict())
-print(list(model.state_dict().keys()))
-print(model.names)
 
-with open(pt_file.split('.')[0] + '.wts', 'w') as f:
-    f.write('{}\n'.format(len(model.state_dict().keys())))
-    for k, v in model.state_dict().items():
-        vr = v.reshape(-1).cpu().numpy()
-        f.write('{} {} '.format(k, len(vr)))
-        for vv in vr:
-            f.write(' ')
-            f.write(struct.pack('>f',float(vv)).hex())
-        f.write('\n')
+def date_modified(path=__file__):
+    # return human-readable file modification date, i.e. '2021-3-26'
+    t = datetime.datetime.fromtimestamp(Path(path).stat().st_mtime)
+    return f'{t.year}-{t.month}-{t.day}'
+def git_describe(path=Path(__file__).parent):  # path must be a directory
+    # return human-readable git description, i.e. v5.0-5-g3e25f1e https://git-scm.com/docs/git-describe
+    s = f'git -C {path} describe --tags --long --always'
+    try:
+        return subprocess.check_output(s, shell=True, stderr=subprocess.STDOUT).decode()[:-1]
+    except subprocess.CalledProcessError as e:
+        return ''  # not a git repository
+def select_device(device='', batch_size=None):
+    # device = 'cpu' or '0' or '0,1,2,3'
+    s = f'YOLOv5 🚀 {git_describe() or date_modified()} torch {torch.__version__} '  # string
+    cpu = device.lower() == 'cpu'
+    if cpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # force torch.cuda.is_available() = False
+    elif device:  # non-cpu device requested
+        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
+        assert torch.cuda.is_available(), f'CUDA unavailable, invalid device {device} requested'  # check availability
 
-#    f.write('{}\n'.format(len(model.names)))
-#    for name in model.names:
-#        f.write(name)
-#        f.write('\n')
+    cuda = not cpu and torch.cuda.is_available()
+    if cuda:
+        n = torch.cuda.device_count()
+        if n > 1 and batch_size:  # check that batch_size is compatible with device_count
+            assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
+        space = ' ' * len(s)
+        for i, d in enumerate(device.split(',') if device else range(n)):
+            p = torch.cuda.get_device_properties(i)
+            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"  # bytes to MB
+    else:
+        s += 'CPU\n'
+
+    logger.info(s.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else s)  # emoji-safe
+    return torch.device('cuda:0' if cuda else 'cpu')
+
+if __name__ =='__main__':
+    logger = logging.getLogger(__name__)
+    # Initialize
+    device = select_device('cpu')
+    pt_file = "yolov5s.pt" # sys.argv[1]
+    # Load model
+    model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
+    model.to(device).eval()
+    print(list(model.state_dict().keys()))
+    print(model.names)
+    with open(pt_file.split('.')[0] + '.wts', 'w') as f:
+        f.write('{}\n'.format(len(model.state_dict().keys())))
+        for k, v in model.state_dict().items():
+            vr = v.reshape(-1).cpu().numpy()
+            f.write('{} {} '.format(k, len(vr)))
+            for vv in vr:
+                f.write(' ')
+                f.write(struct.pack('>f',float(vv)).hex())
+            f.write('\n')
+
